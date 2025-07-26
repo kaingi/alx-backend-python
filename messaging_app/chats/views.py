397d162072458507a4c3588c5178b451945e1097
kaingi <kaingidaudi@gmail.com>
2025-07-26 22:02:57 +0300

    serial7

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Conversation, Message, User
+from .serializers import (
+    ConversationSerializer,
+    MessageSerializer,
+    MessageCreateSerializer
+)
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    def create(self, request, *args, **kwargs):
+        # Accept a list of user_ids to start a new conversation
+        participant_ids = request.data.get('participant_ids', [])
+        if not participant_ids or len(participant_ids) < 2:
+            return Response(
+                {"error": "At least two participants are required."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        participants = User.objects.filter(user_id__in=participant_ids)
+        if participants.count() != len(participant_ids):
+            return Response(
+                {"error": "Some user_ids are invalid."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        conversation = Conversation.objects.create()
+        conversation.participants.set(participants)
+        conversation.save()
+
+        serializer = self.get_serializer(conversation)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.select_related('conversation', 'sender').all()
+    serializer_class = MessageSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return MessageCreateSerializer
+        return MessageSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = MessageCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            message = serializer.save()
+            return Response(MessageSerializer(message).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
